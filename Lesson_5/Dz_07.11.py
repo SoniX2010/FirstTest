0ca63c97e9209f269a64b4695d1fdf464d1f8684
dsoni <gfd6473648@gmail.com>
2023-11-07 16:50:19 +0200

    Dz

---
+++ Lesson_5/Dz_07.11.py
@@ -0,0 +1,15 @@
+from colorama import Fore, Back,Style
+#Fore - задає колір тексту
+print(Fore.RED+ "Red text")
+print("some text")
+#Back - задный фон
+print(Fore.GREEN+ "Green text")
+print("some text")
+#print - виводить текст
+print(Fore.BLUE+ "Blue text")
+print("some text")
+print(Style.DIM+ "Dim text")
+print(Style.RESET_ALL)
+#Style.RESET_ALL- скидає все
+print("some text")
+
